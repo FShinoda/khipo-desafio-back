a588e9ec7037fed29fe4024c9513372309a18e6c
FShinoda <fernanda.shinoda@hotmail.com>
2024-09-05 11:44:40 -0300

    feat: initial project structure

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './product/product.entity';
+import { ProductModule } from './product/product.module';
+
+
+@Module({
+	imports: [
+		TypeOrmModule.forRoot({
+			type: 'mysql',
+			host: 'localhost',
+			port: 3306,
+			username: 'root',
+			password: 'root',
+			database: 'khipo',
+			entities: [Product],
+			synchronize: true
+		}),
+		ProductModule
+	],
+	controllers: [AppController],
+	providers: [AppService],
+})
+export class AppModule {}
